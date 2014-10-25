2a1b4824e6b383eaaefe2f6ae7ded77c796b529e
njarvis <npjarvis@uw.edu>
2014-10-25 16:23:23 -0700

    populated states, hide/show on other, confirm exit

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,49 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+"use strict";
+
+function onReady () {
+    var states = usStates;
+    var signupForm = document.getElementById("signup");
+    var stateSelect = signupForm.elements["state"];
+    var idx;
+    var option;
+
+    for (idx=0; idx<usStates.length; ++idx) {
+        option = document.createElement('option');
+        option.innerHTML = usStates[idx].name;
+        option.value = usStates[idx].code;
+        stateSelect.appendChild(option);
+    }
+
+    var noThanks = document.getElementById('cancelButton');
+
+    document.addEventListener('change', otherOccupation);
+    noThanks.addEventListener('click', function (){
+       if(window.confirm('Are you really sure you want to leave my awesome page?')){
+           window.location = "http://google.com"
+       }
+    });
+}
+
+function otherOccupation () {
+    var occupation = document.getElementById('occupation');
+    var signupForm = document.getElementById("signup");
+    var other = signupForm.elements['occupationOther'];
+    if (occupation.value == "other"){
+        other.style.display = "block";
+    }else {
+        other.style.display = 'none';
+    }
+    return occupation;
+}
+
+function validateForm () {
+    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', onReady);
