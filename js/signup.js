bf836dfb657ae55df566a6f734151a11e05ce7c9
njarvis <npjarvis@uw.edu>
2014-10-25 21:06:21 -0700

    figuring out how to validate the form

--- js/signup.js
+++ js/signup.js
@@ -18,9 +18,9 @@ function onReady () {
         stateSelect.appendChild(option);
     }
 
-    var noThanks = document.getElementById('cancelButton');
-
     document.addEventListener('change', otherOccupation);
+    signupForm.addEventListener('submit', onSubmit);
+    var noThanks = document.getElementById('cancelButton');
     noThanks.addEventListener('click', function (){
        if(window.confirm('Are you really sure you want to leave my awesome page?')){
            window.location = "http://google.com"
@@ -40,11 +40,57 @@ function otherOccupation () {
     return occupation;
 }
 
-function validateForm () {
-    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+function onSubmit (evt) {
+    //var valid = true;
+    //try {
+    //    valid = validateForm(this);
+    //}
+    //catch(exception) {
+    //    console.log(exception);
+    //    valid = false;
+    //}
+    evt.returnValue = validateForm(this);
+    if (!evt.returnValue && evt.preventDefault) {
+        evt.preventDefault();
+    }
+    return evt.returnValue;
 }
 
+function validateForm (form) {
+    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+    if (occupation.value == 'other') {
+        requiredFields.add(other);
+    } else {
 
+    }
+    var idx;
+    var formValid = true;
+    for(idx = 0; idx < requiredFields.length; ++idx) {
+        formValid &= validateRequiredField(form.elements[requiredFields[idx]]);
+    }
+
+    var occupation = document.getElementById('occupation');
+    var signupForm = document.getElementById("signup");
+    var other = signupForm.elements['occupationOther'];
+
+    if (!formValid) {
+        var errMsg = document.getElementById('error-message');
+        errMsg.innerHTML = 'YOU MUST FILL OUT REQUIRED FIELDS';
+        errMsg.style.display = 'block';
+    }
+    return formValid;
+}
+
+function validateRequiredField(field) {
+    var value = field.value.trim();
+    var validVal = value.length > 0;
+    if (validVal) {
+        field.className = 'form-control';
+    } else {
+        field.className = 'form-control invalid'
+    }
+    return validVal;
+}
 
 
 document.addEventListener('DOMContentLoaded', onReady);
